e75270c532b07204bb89b2c0f8b5ffb1f66cb46f
Nick Randell <nick.randell@sceneskope.com>
2020-01-24 15:54:24 +0000

    Updates

--- Docker/openvinocvsharp/build.sh
+++ Docker/openvinocvsharp/build.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
-docker build -t "nrandell/openvinocvsharp:build" --build-arg NUGET_API_KEY=$NUGET_API_KEY -f Dockerfile .
+. ../variables.sh
+
+docker build -t "nrandell/openvinocvsharp:build" --build-arg OPENCVSHARPEXTERN_VERSION=${OPENCVSHARPEXTERN_VERSION} --build-arg OPENCVSHARP_VERSION=${OPENCVSHARP_VERSION} --build-arg NUGET_API_KEY=$NUGET_API_KEY -f Dockerfile .
 
 
--- StandaloneFaceDetection/build.sh
+++ StandaloneFaceDetection/build.sh
@@ -5,6 +5,6 @@ set -evx
 
 . ../Docker/variables.sh
 
-OUTPUT_VERSION="1.11"
+OUTPUT_VERSION="1.14"
 
 docker buildx build --platform linux/arm64,linux/arm/v7,linux/amd64 --build-arg IMAGE_VERSION=${VERSION} -t "nrandell/standalonefacedetection:${OUTPUT_VERSION}" -f Dockerfile --push ..
