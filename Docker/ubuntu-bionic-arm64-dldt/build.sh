8a30d26094ef34bb0384d58e71e1be0266b63ad3
Nick Randell <nick.randell@sceneskope.com>
2020-02-21 13:24:39 +0000

    New versions

---
+++ Docker/ubuntu-bionic-arm64-dldt/build.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+. ../variables.sh
+
+docker buildx build --platform linux/arm64 --build-arg DLDT_VERSION=${DLDT_VERSION} -t "${BASE_DLDT_IMAGE}:${DLDT_ARM64_VERSION}" -f Dockerfile --load .
+
+
